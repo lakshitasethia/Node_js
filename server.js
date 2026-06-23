acc2460281bf2b2e358d1c7719a45897fff4016d
lakshitasethia <ksethia1978@gmail.com>
2026-06-24 00:36:16 +0530

    new files added

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 import http from 'http';
-
+const PORT = process.env.PORT || 5002;
 const server = http.createServer((req, res) => {
-    // res.setHeader('Content-Type', 'text/html');
-    // res.statusCode = 404;
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end("<button>Hello, World!</button>");
+    try {
+        if (req.method === 'GET') {
+            if (req.url === '/') {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end("<h1>Homepage!</h1>");
+            } else if (req.url === '/about') {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end("<h1>About!</h1>");
+            } else {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end("<h1>404 Not Found</h1>");
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end("<h1>Method Not Allowed</h1>");
+        }
+    }
+    catch (err) {
+        res.writeHead(500, { 'Content-Type': 'text/html' });
+        res.end("<h1>Internal Server Error</h1>");
+    }
+
 });
-server.listen(5002, () => {
-    console.log("Server is running on port 5002");
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
